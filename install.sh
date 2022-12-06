0b9f746572b0544cccbe0a2974767794024d3d85
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-12-06 16:46:22 +0200

    Add GHCR build actions

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
 
 # Run the virtual machine
-exec sudo docker run -d --restart always --device=/dev/kvm --cap-add=net_admin --network host bp
+exec sudo docker run -d --restart always --device=/dev/kvm --cap-add=net_admin --network host ghcr.io/maggie0002/op-brave-potato:latest

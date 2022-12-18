44a00f0fc57c227a309513f8f0bdcbdb73ac292f
maggie0002 <64841595+maggie0002@users.noreply.github.com>
2022-12-18 15:36:55 +0000

    Update IPS

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
 curl -fsSL https://get.docker.com | sudo sh
 
 # Install Tailscale
-export TS_ROUTES=10.0.3.0/24
+# Use `tailscale up --advertise-routes=10.0.3.0/24 --accept-routes`` to enable subnets
 curl -fsSL https://tailscale.com/install.sh | sudo sh
 echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
 echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.d/99-tailscale.conf
